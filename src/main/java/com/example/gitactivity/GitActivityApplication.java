194358e410c993e705af634f3f7fea57c408a866
dYalmaz <yalmaz.dzhengiz@gmail.com>
2026-08-31 09:38:01 +0100

    Initial commit

---
+++ src/main/java/com/example/gitactivity/GitActivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitactivity/GitActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
