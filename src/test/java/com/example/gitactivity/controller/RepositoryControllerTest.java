60a44d0c8d5d4be3daf53df5656daa2741af26e3
dYalmaz <yalmaz.dzhengiz@gmail.com>
2026-09-24 12:39:44 +0100

    added controller tests

--- src/test/java/com/example/gitactivity/controller/RepositoryControllerTest.java
+++ src/test/java/com/example/gitactivity/controller/RepositoryControllerTest.java
@@ -1,13 +1,16 @@
 package com.example.gitactivity.controller;
 
 import com.example.gitactivity.dto.GitHubRepositoryResponse;
+import com.example.gitactivity.exception.GitHubRateLimitException;
+import com.example.gitactivity.exception.GitHubServiceException;
+import com.example.gitactivity.exception.RepositoryNotFoundException;
 import com.example.gitactivity.service.RepositoryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class RepositoryControllerTest {
 
@@ -32,7 +35,42 @@ public class RepositoryControllerTest {
 
         assertEquals("spring-boot", response.getName());
 
+        verify(repositoryService).getRepoDetails("spring-projects", "spring-boot");
+
+
+
     }
+
+    @Test
+    void getRepositoryShouldPropagateRepositoryNotFoundException(){
+
+        when(repositoryService.getRepoDetails("invalid-owner","invalid-repo")).thenThrow(new RepositoryNotFoundException("Repository not found"));
+
+        assertThrows(RepositoryNotFoundException.class, () -> {
+            repositoryController.getRepository("invalid-owner", "invalid-repo");
+        });
+
+    }
+
+    @Test
+    void getRepositoryShouldPropagateRateLimitExceededException(){
+
+        when(repositoryService.getRepoDetails("spring-projects", "spring-boot")).thenThrow(new GitHubRateLimitException("Rate limit exceeded"));
+
+        assertThrows(GitHubRateLimitException.class, () -> {repositoryController.getRepository("spring-projects", "spring-boot");});
+
+    }
+
+    @Test
+    void getRepositoryShouldPropagateGitHubServiceException(){
+
+        when(repositoryService.getRepoDetails("spring-projects", "spring-boot")).thenThrow(new GitHubServiceException("GitHub service error"));
+
+        assertThrows(GitHubServiceException.class, () -> {repositoryController.getRepository("spring-projects", "spring-boot");});
+
+    }
+
+
 
 
 }
